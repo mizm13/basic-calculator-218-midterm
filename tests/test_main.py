a6e71a5781f9e9787d8620cf0bc1e4c8e790cc52
mizm13 <mizanmodak@gmail.com>
2024-10-23 00:41:43 -0400

    cleaned up file for test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,5 @@
 # File: test_main.py
-import sys
-import os
-
-# Add the parent directory (project root) to sys.path so 'main.py' can be imported
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-# Now you can import the 'add' function
-from main import add
+from app.main import add
 
 def test_add():
     assert add(1, 2) == 3
