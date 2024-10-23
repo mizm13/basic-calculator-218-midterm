e5c03f098766716c178d9e7087706466ea4ab4ef
mizm13 <mizanmodak@gmail.com>
2024-10-23 00:49:47 -0400

    fixed documentation so now pylint passes

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,8 @@
-# File: test_main.py
+""" File: test_main.py """
 from app.main import add
 
 def test_add():
+    """Addition function"""
     assert add(1, 2) == 3
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
