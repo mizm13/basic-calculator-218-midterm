4899b6acf158ba334f6d7739744087cb0ff37945
mizm13 <mizanmodak@gmail.com>
2024-10-25 01:12:31 -0400

    added test cases for each operation

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,23 @@
 """ File: test_main.py """
-from app.main import add
+from app.main import add, subtract, multiply, divide
 
 def test_add():
     """Addition function"""
     assert add(1, 2) == 3
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+    
+def test_subtract():
+    """Addition function"""
+    assert subtract(1, 1) == 0
+    
+    
+def test_multiply():
+    """Multiply function"""
+    assert multiply(1, 3) == 3
+    
+
+def test_divide():
+    """Divide function"""
+    assert divide(4, 2) == 2
+
