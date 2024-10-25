08b64251283fb563719a5471a27c805c691fe11e
mizm13 <mizanmodak@gmail.com>
2024-10-25 01:12:16 -0400

    added subtraction, multiply, and divide

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
 # Simple add function
 def add(a, b):
     return a + b
+
+# Simple add function
+def subtract(a, b):
+    return a - b
+
+# Simple add function
+def multiply(a, b):
+    return a * b
+
+# Simple add function
+def divide(a, b):
+    return a / b
