029a2c868b3f89385d6e584a969df02f6e6a218c
mizm13 <mizanmodak@gmail.com>
2024-11-08 12:10:23 -0500

    moved file and renamed folder

--- app/main.py
+++
@@ -1,6 +0,0 @@
-# Import the calculator function from app/calculator.py
-from app.calculator import calculator
-
-# If this script is run directly, start the calculator.
-if __name__ == "__main__":
-    calculator()
